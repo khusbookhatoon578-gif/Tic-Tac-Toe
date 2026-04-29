051e5a445ce44431ee14816d8241c785831f74d2
khusbookhatoon578-gif <khusbookhatoon578@gmail.com>
2026-04-29 15:27:00 +0530

    UC3:Accept User Slot Input (1-9)

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
+    }
+
+    /**
+     * Reads an integer slot value from the user
+     */
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+
+        return slot;
+    }
+}
+
