492ec4b86d9f9649db219c266c3afb27525fd04c
khusbookhatoon578-gif <khusbookhatoon578@gmail.com>
2026-05-01 18:09:00 +0530

    UC7 Computer makes random move easy level

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,14 +1,54 @@
+import java.util.Random;
+
+/**
+ * TicTacToe
+ * UC7 allows the computer to make a random valid move
+ * by reusing slot conversion and validation logic.
+ */
 public class TicTacToe {
 
-    static char[][] board = new char[3][3];
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
 
+    static char computerSymbol = 'O';
 
+    /**
+     * Entry point of the program. Triggers the computer move.
+     */
     public static void main(String[] args) {
-        placeMove(0, 0, 'X');
-        System.out.println(board[0][0]);
+        computerMove();
+        printBoard();
     }
 
-    static void placeMove(int row, int col, char symbol) {
-        board[row][col] = symbol;
+    /**
+     * Generates random slot values until a valid move is found,
+     * then places the computer symbol on the board.
+     */
+    static void computerMove() {
+        Random rand = new Random();
+        int row, col;
+        while (true) {
+            row = rand.nextInt(3);
+            col = rand.nextInt(3);
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                break;
+            }
+        }
+    }
+
+    /**
+     * Prints the current state of the board.
+     */
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
